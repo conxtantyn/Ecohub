f7092782d224e322add3f59577c78d14b279bcb8
conxtantyn <conxtantyn@gmail.com>
2026-03-11 10:47:41 +0100

    chore(project): remove redundant resource

--- core/ui/src/commonMain/kotlin/com/ecohub/ui/extension/String.kt
+++
@@ -1,9 +0,0 @@
-package com.ecohub.ui.extension
-
-fun String.format(vararg args: Any?): String {
-    var formatted = this
-    args.forEachIndexed { index, arg ->
-        formatted = formatted.replace("{$index}", arg.toString())
-    }
-    return formatted
-}
